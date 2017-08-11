bdbcfccc3f0ddf5b7724ab16e3ed1e63118c353b
Steven <shockshocksoul@gmail.com>
2017-08-11 17:10:54 +0800

    ** Upadte ** New pages prepare it up.

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Vendor;
+
+class StatusController extends Controller
+{
+    //
+
+    public function index($slug){
+		$vendor = Vendor::where('slug', $slug)->first();
+    	return view('status')->with('vendor', $vendor);
+    }
+}
