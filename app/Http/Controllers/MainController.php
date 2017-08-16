2512094c2959a2aad0d2f5d2c775942fbfb438c9
Steven <shockshocksoul@gmail.com>
2017-08-16 11:29:03 +0800

    ** Update ** update 404 not found route, and 404 return link.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,8 +11,12 @@ class MainController extends Controller
     public function index($slug) {
 
 		$vendor = Vendor::where('slug', $slug)->first();
+		if($vendor){
 		return view('main')->with('vendor', $vendor);
-
+		}
+		else{
+			abort(404);
+		}
     }
 
 }
