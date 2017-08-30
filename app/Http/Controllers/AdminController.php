f51183d4556c545c9100ea9f70473c1878e0471a
Steven <shockshocksoul@gmail.com>
2017-08-30 17:33:04 +0800

    ** Add ** Add a backstage function lists for more features visiblity.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use Excel;
+use Carbon\Carbon;
+
 use Illuminate\Http\Request;
 use App\Models\Container_type;
 use App\Models\Container;
 use App\Models\Vendor;
+use App\Models\Customer;
 
 class AdminController extends Controller
 {
     public function index(){
-            $container = Container::Paginate(15);
-    	    return view('admin')->with('container',$container);
+            ;
+    	    return view('admin');
     }
 
     public function list(){
@@ -44,4 +47,53 @@ class AdminController extends Controller
    return view("container_type")->with('status',$status);
          
 	}
+
+    public function exportFile(){
+
+        
+
+Excel::create('ContainerHistory-'.Carbon::now(), function($excel) {
+        $excel->setTitle('好盒器容器使用紀錄');
+
+        $excel->setCreator('Steven Ho')
+          ->setCompany('好盒器');
+
+        $excel->sheet('Sheet 1', function($sheet) {
+
+            $containers = Container::all();
+                foreach($containers as $container) {
+                    $container_type = Container_type::find($container->number)->decoration;
+                 $data[] = array(
+                    $container->id,
+                    $container->number,
+                    $container_type,
+                    $container->vendor_name,
+                    $container->customer_phone,
+                    ($container->status == 1 ? '出借' : '歸還'),
+                    $container->created_at,
+                );
+            }
+            $sheet->fromArray($data, null, 'A1', false, false);
+            $headings = array('資料代號', '杯子號碼', '杯子容量', '店家名稱', '顧客手機', '杯子狀態','租借用時間');
+            $sheet->prependRow(1, $headings);
+            $sheet->setAutoSize(true);
+        });
+    })->download('csv');
+    }
+
+
+    public function record(){
+        $container = Container::Paginate(15);
+        return view('record')->with('container',$container);
+    }
+
+    public function status(){
+        $vendor = Vendor::all()->count() - 1;
+        $container_lend = Container::where('status' ,'=', 1)->count();
+        $container_recover = Container::where('status' ,'=', 0)->count();
+        $container_type = Container_type::all()->count() - 10;
+        $customer = Customer::all()->count();
+
+        return view('bstatus',compact('vendor','container_lend','container_recover','container_type','customer'));
+    }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,44 +4,12 @@
 	<div class="container">
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4 text-center">
-			<a href="#"><button class="gtg-button button-small">下載 CSV 檔</button></a>
+			<a href="{{ Route('record') }}"><button class="gtg-button button-small">顯示所有容器紀錄</button></a>
+			<a href="{{ Route('export') }}"><button class="gtg-button button-small">下載 CSV 檔</button></a>
+			<a href="{{ Route('bstatus') }}"><button class="gtg-button button-small">統計資料</button></a>
+			<a href="{{ Route('bstatus') }}"><button class="gtg-button button-small">上傳容器列表</button></a>
+			<a href="{{ Route('bstatus') }}"><button class="gtg-button button-small">更新店家配送狀態</button></a>
 		</div>
 
-
-			  <table class="table table-responsive table-striped text-center"  style="margin-top: 24px;">
-				  <thead>
-				    <tr>
-				      <th>代號</th>
-				      <th>杯子</th>
-				      <th>商家</th>
-				      <th>顧客</th>
-				      <th>狀態</th>
-				      <th>時間</th>
-				    </tr>
-				  </thead>
-
-
-				<tbody>
-		@foreach($container as $c)
-   			 <tr>
-     				 	<th scope="row">{{ $c->id }} </th>
-     				 	<td> {{ $c->number }} </td>
-      					<td> {{ substr($c->vendor_name, 0, 6) }} </td>
-      					<td> {{ $c->customer_phone }} </td>
-      					<td> @if($c->status == 0) 歸還 @else 借出 @endif</td>
-      					<td> {{ Carbon\Carbon::parse($c->created_at)->format('m/d h:i') }} </td>
-    			</tr>
-
-
-    				@endforeach
-  				</tbody>
-
-
-			  </table>
-			
-				<div class="container text-center">
-					{{ $container->links() }}
-				</div>
-	</div>
 </div>
 @stop
